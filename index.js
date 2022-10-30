cad58a86c30c6a305534ce7ac84cb11bbccbab91
Adam Kiss <iam@adamkiss.com>
2022-10-30 16:19:13 +0100

    Remove the proxying code

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ panel.plugin("adamkiss/goat-counter", {
 		'k-goat-counter-view': {
 			template: `
 				<k-inside>
-					<k-view class="k-goat-counter-view" style="padding-top: 20px; background-color: white;">
-						<iframe v-if="siteName" src="/adamkiss/goat-counter-proxy" frameborder="0" class="goat-counter-iframe"></iframe>
+					<k-view class="k-goat-counter-view">
+						<iframe v-if="siteName" v-bind:src="'https://' + siteName + '.goatcounter.com?hideui=1&access-token=' + token" frameborder="0" class="goat-counter-iframe"></iframe>
 						<div style="margin-top: 30px; text-align: center;" v-else>
-							<code>You need to set adamkiss.goat-counter.site-name in config.php</code>
+							<code>You need to set 'adamkiss.goat-counter.site-name' in config.php</code>
 						</div>
 					</k-view>
 				</k-inside>
--- index.php
+++ index.php
@@ -30,45 +30,4 @@ Kirby::plugin('adamkiss/goat-counter', [
 	'snippets' => [
 		'goat-counter' => __DIR__ . '/snippets/goat-counter-embed.php'
 	],
-	'routes' => [
-		[
-			// try to get around the Cookie->Redirect pattern by proxying the token view
-			'pattern' => 'adamkiss/goat-counter-proxy',
-			'action' => function() {
-				$siteName = option('adamkiss.goat-counter.site-name');
-				$token = option('adamkiss.goat-counter.token');
-
-				$goatCounter = Remote::get("https://{$siteName}.goatcounter.com?hideui=1", [
-					'headers' => [
-						'Cookie' => "access-token={$token}"
-					],
-				]);
-
-				$goatCounterProxied = preg_replace('/\/\/static.zgo.at\/(.*?)js(.*?)"/', '/adamkiss/goat-counter-proxy/$1js$2"', $goatCounter->content());
-
-				return $goatCounterProxied;
-			},
-		],
-		[
-			// try to get around the Cross-Origin with the javascript
-			'pattern' => 'adamkiss/goat-counter-proxy/(:any)',
-			'action' => function(string $file) {
-				$siteName = option('adamkiss.goat-counter.site-name');
-
-				if (str_contains($file, 'js')) {
-					$content = Remote::get("https://static.zgo.at/{$file}?v=".kirby()->request()->query()->get('v'))->content();
-
-					if (str_contains($file, 'dashboard')) {
-						$content = str_replace(
-							"window.WEBSOCKET = new WebSocket((location.protocol === 'https:' ? 'wss://' : 'ws://') + document.location.host + '/loader?id=' + cid)",
-							"window.WEBSOCKET = new WebSocket('wss://{$siteName}.goatcounter.com/loader?id=' + cid)",
-							$content
-						);
-					}
-
-					return new Response($content, 'text/javascript');
-				}
-			},
-		]
-	],
 ]);
