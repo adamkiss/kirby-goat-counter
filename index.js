cee33a424b5272feb1dc070869053fa631215377
Adam Kiss <iam@adamkiss.com>
2022-10-31 14:47:22 +0100

    Add the notification about the possible issue and add a link to the analytics

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ panel.plugin("adamkiss/goat-counter", {
 			template: `
 				<k-inside>
 					<k-view class="k-goat-counter-view">
+						<div v-if="siteName" style="padding-top: 10px; text-align: center">There's currently an issue with the Goat Counter where the "access-token → redirect" pattern might be blocked by your browser. You can <a v-bind:href="'https://' + siteName + '.goatcounter.com?hideui=1&access-token=' + token" target="_blank" style="text-decoration: underline;">check the analytics</a> in a new tab instead.</div>
 						<iframe v-if="siteName" v-bind:src="'https://' + siteName + '.goatcounter.com?hideui=1&access-token=' + token" frameborder="0" class="goat-counter-iframe"></iframe>
 						<div style="margin-top: 30px; text-align: center;" v-else>
 							<code>You need to set 'adamkiss.goat-counter.site-name' in config.php</code>
